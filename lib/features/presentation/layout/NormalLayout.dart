9fac90578c048b5b82c52a1d5b269cb7904be657
Don Carlo <plam65731@gmail.com>
2025-06-28 16:04:04 +0700

    Implement : Menu bar

--- lib/features/presentation/layout/NormalLayout.dart
+++ lib/features/presentation/layout/NormalLayout.dart
@@ -13,6 +13,77 @@ class NormalLayout extends StatelessWidget {
       body: Center(
         child: Home(),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Action to perform when the button is pressed
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add',
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                // Action to perform when the home icon is pressed
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Action to perform when the search icon is pressed
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                // Action to perform when the notifications icon is pressed
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {
+                // Action to perform when the account icon is pressed
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
